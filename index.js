0213f9b7749c1d52cf06a0a687b25f45f3afde1c
Specticall <josephyusmita@gmail.com>
2023-08-04 14:32:57 +0700

    about to fix the mobile nav bug

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Todo :
+/*
+3. Mobile nav menu
+4. footer belom di tambahin highlight class.
+*/
+
 // Desktop Features Section Scroll
 const feature_buttons = document.querySelector(
   ".features__buttons"
@@ -7,6 +13,9 @@ const feature_desktop_indicator = document.querySelector(
 );
 const feature_breakpoint = window.matchMedia(
   "(max-width:690px)"
+);
+const feature_tabs = document.querySelectorAll(
+  ".features__tabbed"
 );
 
 feature_buttons.addEventListener(
@@ -21,7 +30,6 @@ function tabbed_handler({ isCallback = true }) {
   const initial = feature_buttons.children[0];
 
   function desktop_style(clicked, remove = false) {
-    console.log("desktop");
     // Measurement stuff
     const element_width =
       clicked.getBoundingClientRect().width;
@@ -41,7 +49,6 @@ function tabbed_handler({ isCallback = true }) {
   }
 
   function mobile_style(clicked, remove = false) {
-    console.log("mobile");
     [...feature_buttons.children].forEach((btn) => {
       btn.classList.remove("highlight-text");
       btn.classList.remove("highlight-indicator");
@@ -73,7 +80,19 @@ function tabbed_handler({ isCallback = true }) {
     feature_breakpoint.matches
       ? mobile_style(clicked)
       : desktop_style(clicked);
+
+    switch_tab(clicked.dataset.tab);
   };
+}
+
+function switch_tab(destination) {
+  feature_tabs.forEach((tab) => {
+    tab.classList.add("features--hidden");
+  });
+
+  feature_tabs[destination].classList.remove(
+    "features--hidden"
+  );
 }
 
 // Screen size resize event listener
@@ -91,4 +110,100 @@ window.addEventListener("resize", () => {
       btn.classList.remove("highlight-text");
       btn.classList.remove("highlight-indicator");
     });
+});
+
+// Accordion
+const accordion_wrappers = document.querySelectorAll(
+  ".accordion-wrapper"
+);
+const accordion_container =
+  document.querySelector(".accordion");
+// const accordions =
+
+accordion_container.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".accordion__block");
+
+  if (!clicked) return;
+
+  const faq_index = clicked.dataset.faq - 1;
+
+  accordion_wrappers.forEach((wrapper) =>
+    wrapper.classList.add("accordion--hidden")
+  );
+
+  accordion_wrappers[faq_index].classList.remove(
+    "accordion--hidden"
+  );
+});
+
+// Search Bar
+const signup_input = document.querySelector(
+  ".signup__input"
+);
+const signup_submit = document.querySelector(
+  ".signup__submit"
+);
+const signup_container = document.querySelector(
+  ".signup__input-container"
+);
+
+let countdown;
+signup_submit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const input_value = signup_input.value;
+
+  // if input is NOT EMAIL, the display the wrong msg
+  if (!input_isEmail(input_value)) {
+    signup_container.classList.add("wrong-email");
+
+    // Reset the countdown so that it doesn't overlap
+    clearInterval(countdown);
+
+    // Sets the countdown timer, after 1000ms, the popup disappears
+    countdown = setTimeout(() => {
+      signup_container.classList.remove("wrong-email");
+    }, 4000);
+  } else {
+  }
+});
+
+function input_isEmail(input) {
+  const regex =
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(input);
+}
+
+// Scroll Reveal
+const sections = document.querySelectorAll(".section");
+const images = document.querySelectorAll(".images");
+
+function scroll_reveal(entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove("invisible");
+
+    observer.unobserve(entry.target);
+  });
+}
+const scroll_reveal_observer = new IntersectionObserver(
+  scroll_reveal,
+  { root: null, threshold: 0.2 }
+);
+
+sections.forEach((section) =>
+  scroll_reveal_observer.observe(section)
+);
+images.forEach((image) =>
+  scroll_reveal_observer.observe(image)
+);
+
+// Mobile Open navigation
+const mobile_menu_toggle = document.querySelector(
+  ".mobile_nav-button"
+);
+
+mobile_menu_toggle.addEventListener("click", function (e) {
+  this.closest(".nav").classList.toggle("mobile-open");
 });
